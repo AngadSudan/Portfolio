83885c1eb2184213d2684594c1c5f4ff0343eb3d
AngadSudan <angadsudan453@gmail.com>
2024-07-08 12:15:34 +0530

    removed faulty animations

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,8 +2,19 @@
 module.exports = {
   content: ['./index.html'],
   theme: {
-    extend: {},
+    extend: {
+      keyframes: {
+        gradientFill: {
+          '0%': { backgroundPosition: '0% 50%',
+              
+           },
+          '100%': { backgroundPosition: '100% 50%' },
+        },
+      },
+      animation: {
+        gradientFill: 'gradientFill 5s linear infinite',
+      },
+    },
   },
   plugins: [],
 }
-
